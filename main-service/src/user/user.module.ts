4b0eadb6054fe5cf855e8e5c8db8678242d74e7a
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2022-01-27 18:04:49 -0300

    feat(main-service): restore user module

---
+++ main-service/src/user/user.module.ts
@@ -0,0 +1,28 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { UserService } from './user.service';
+import { AuthMiddleware } from '../common/middlewares/auth.middleware';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
+})
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(
+        { path: 'users', method: RequestMethod.GET },
+        { path: 'users', method: RequestMethod.DELETE },
+      );
+  }
+}
